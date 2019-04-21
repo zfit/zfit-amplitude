Fix wrong axis in kinematics

examples/d2kpipi0.py
@@ -8,16 +8,16 @@
 """Implementation of the D0 -> K+ pi- pi0 Dalitz from PRL 103 (2009) 211801."""
 
 from math import radians
+import tensorflow as tf
 
 from particle.particle import literals as lp
 from particle import Particle
 
 import zfit
 
-from zfit_amplitude.amplitude import Decay, Amplitude, Resonance
+from zfit_amplitude.amplitude import Decay, Amplitude, Resonance, SumAmplitudeSquaredPDF
 import zfit_amplitude.dynamics as dynamics
 import zfit_amplitude.kinematics as kinematics
-
 
 polar_param = zfit.ComplexParameter.from_polar
 
@@ -27,19 +27,14 @@ PI_ZERO = lp.pi_0
 K_PLUS = lp.K_plus
 D_ZERO = lp.D_0
 
-
-RESONANCES = {'rho(770)': ('m2pipi', Resonance(lp.rho_770_plus,
-                                               dynamics.RelativisticBreitWignerReal)),
-              'K2*(1430)0': ('m2kpim', Resonance(Particle.from_string('K(2)*(1430)'),
-                                                 dynamics.RelativisticBreitWignerReal)),
-              'K0*(1430)+': ('m2kpi0', Resonance(lp.K_0st_1430_plus,
-                                                 dynamics.RelativisticBreitWignerReal)),
-              'K*(892)+': ('m2kpi0', Resonance(lp.Kst_892_plus,
-                                               dynamics.RelativisticBreitWignerReal)),
-              'K0*(1430)0': ('m2kpim', Resonance(lp.K_0st_1430_0,
-                                                 dynamics.RelativisticBreitWignerReal)),
-              'K*(892)0': ('m2kpim', Resonance(lp.Kst_892_0,
-                                               dynamics.RelativisticBreitWignerReal))}
+RESONANCES = {
+    # 'rho(770)': ('m2pipi', Resonance(lp.rho_770_plus, dynamics.RelativisticBreitWignerReal)),
+    # 'K2*(1430)0': ('m2kpim', Resonance(Particle.from_string('K(2)*(1430)'), dynamics.RelativisticBreitWignerReal)),
+    # 'K0*(1430)+': ('m2kpi0', Resonance(lp.K_0st_1430_plus,dynamics.RelativisticBreitWignerReal)),
+    'K*(892)+': ('m2kpi0', Resonance(lp.Kst_892_plus, dynamics.RelativisticBreitWignerReal)),
+    # 'K0*(1430)0': ('m2kpim', Resonance(lp.K_0st_1430_0, dynamics.RelativisticBreitWignerReal)),
+    'K*(892)0': ('m2kpim', Resonance(lp.Kst_892_0,dynamics.RelativisticBreitWignerReal))
+}
 
 COEFFS = {'rho(770)': polar_param('f_rho770', 1.0, 0.0, floating=False),
           'K2*(1430)0': polar_param('f_K2star1430_0', 0.088, radians(-17.2)),
@@ -61,7 +56,7 @@ class D2Kpipi0Amplitude(Amplitude):
 
     """
 
-    def __init__(self, resonance, resonance_model):  # noqa
+    def __init__(self, resonance: str, resonance_model):  # noqa
         self.mass_var, self.resonance = RESONANCES[resonance]
         res_mass = self.resonance.mass_sampler(self.resonance.name,
                                                mres=self.resonance.mass, wres=self.resonance.width,
@@ -78,12 +73,14 @@ class D2Kpipi0Amplitude(Amplitude):
                     ('K+', K_PLUS.mass, []),
                     ('pi0', PI_ZERO.mass, [])]),
                 ('pi-', PI_MINUS.mass, [])])
-        else:
+        elif self.mass_var == 'm2kpi0':
             decay_tree = ('D0', D_ZERO.mass, [
                 (self.resonance.name, res_mass, [
                     ('K+', K_PLUS.mass, []),
                     ('pi-', PI_MINUS.mass, [])]),
                 ('pi0', PI_ZERO.mass, [])])
+        else:
+            raise ValueError(f"Invalid mass_var {self.mass_var}.")
         super().__init__(decay_tree)
 
     def _amplitude(self, obs):
@@ -94,19 +91,25 @@ class D2Kpipi0Amplitude(Amplitude):
 
 def var_transformation(self, particles):
     """Transform particles to our observables of interest."""
-    return {'m2kpim': kinematics.mass_squared(particles['K+']+particles['pi-']),
-            'm2kpi0': kinematics.mass_squared(particles['K+']+particles['pi0']),
-            'm2pipi': kinematics.mass_squared(particles['pi-']+particles['pi0'])}
+    return {'m2kpim': kinematics.mass_squared(tf.transpose(particles['K+'] + particles['pi-'])),
+            'm2kpi0': kinematics.mass_squared(tf.transpose(particles['K+'] + particles['pi0'])),
+            'm2pipi': kinematics.mass_squared(tf.transpose(particles['pi-'] + particles['pi0']))}
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+
+    zfit.settings.set_verbosity(6)
+    # HACK to use default uniform sampling in accept reject
+    SumAmplitudeSquaredPDF._hack_use_default_sampling = True
+
+
     m2kpim = zfit.Space(obs='m2kpim',
-                        limits=((K_PLUS.mass + PI_MINUS.mass)**2, (D_ZERO.mass - PI_ZERO.mass)**2))
+                        limits=((K_PLUS.mass + PI_MINUS.mass) ** 2, (D_ZERO.mass - PI_ZERO.mass) ** 2))
     m2kpiz = zfit.Space(obs='m2kpi0',
-                        limits=((K_PLUS.mass + PI_ZERO.mass)**2, (D_ZERO.mass - PI_MINUS.mass)**2))
+                        limits=((K_PLUS.mass + PI_ZERO.mass) ** 2, (D_ZERO.mass - PI_MINUS.mass) ** 2))
     m2kpipi = zfit.Space(obs='m2pipi',
-                         limits=((PI_ZERO.mass + PI_MINUS.mass)**2, (D_ZERO.mass - K_PLUS.mass)**2))
+                         limits=((PI_ZERO.mass + PI_MINUS.mass) ** 2, (D_ZERO.mass - K_PLUS.mass) ** 2))
 
     masses = m2kpim * m2kpiz * m2kpipi
 
@@ -118,16 +121,26 @@ if __name__ == "__main__":
     for dep in pdf.get_dependents(only_floating=False):
         print("{} {} Floating: {}".format(dep.name, zfit.run(dep), dep.floating))
 
-    sample = pdf.sample(8000)
-    sample_np = zfit.run(sample)
+    integral = pdf.integrate(limits=masses)
+    integral = zfit.run(integral)
+    print(f"Integral (should be 1): {integral}")
+    sample = pdf.sample(20000)
+    sample_np = zfit.run(sample) / (1000 ** 2)  # from MeV^2 to GeV^2
     for axis1, axis2 in ((0, 1), (1, 2), (2, 0)):
         obs1 = pdf.obs[axis1]
         obs2 = pdf.obs[axis2]
         plt.figure()
         plt.title(f"Dalitz plot D0->K+pi-pi0 of {obs1}:{obs2}")
         plt.scatter(sample_np[:, axis1], sample_np[:, axis2], s=0.15)
-        plt.xlabel(obs1)
-        plt.ylabel(obs2)
+        plt.xlabel(f"{obs1} ($GeV^2$)")
+        plt.ylabel(f"{obs2} ($GeV^2$)")
+    for axis in range(3):
+        obs = pdf.obs[axis]
+        plt.figure()
+        plt.hist(sample_np[:, axis], bins=150)
+        plt.title(f"Histogram of {obs}")
+        plt.ylabel(f"counts")
+        plt.xlabel(f"{obs}")
     plt.show()
 
 # EOF

zfit_amplitude/amplitude.py
@@ -55,11 +55,11 @@ class Decay:
         self._var_transforms = variable_transformations
 
     def add_amplitude(self, amplitude, coeff):
-        """Add amplitude and its correspondig coefficient.
+        """Add amplitude and its corresponding coefficient.
 
         Arguments:
             amplitude (:py:class:`Amplitude`): Amplitude to add.
-            coeff (:py:class:~zfit.core.parameter.Parameter): Coefficient corresponding
+            coeff (:py:class:`~zfit.Parameter`): Coefficient corresponding
                 to the amplitude.
 
         Return:
@@ -143,9 +143,9 @@ class AmplitudeProductProjectionCached(BaseFunctorFunc, SessionHolderMixin):
                  **kwargs):  # noqa: W107
         self.projector = projector
         super().__init__(funcs=[amp1, amp2], name=name, obs=None, **kwargs)
-        integral_holder = tf.Variable(initial_value=-42, trainable=False,
+        integral_holder = tf.Variable(name=f"integral_cache_{name}", initial_value=-1e12, trainable=False,
                                       dtype=self.dtype, use_resource=True)
-        # self.sess.run(integral_holder.initializer)
+        self.sess.run(integral_holder.initializer)
         self._product_cache_integral_holder = integral_holder
 
     def _func(self, x):
@@ -157,25 +157,32 @@ class AmplitudeProductProjectionCached(BaseFunctorFunc, SessionHolderMixin):
         return ztf.run_no_nan(func=func, x=x)
 
     def _single_hook_integrate(self, limits, norm_range, name='_hook_integrate'):
-        integral = self._cache.get("integral")
+        # HACK no caching
+        # return super()._single_hook_integrate(limits=limits, norm_range=norm_range, name=name)
+        # HACK to check variable
+        # integral = self._cache.get("integral")
+        integral = None
         if integral is None:
             self._cache['integral'] = {}
+        # TODO(Mayou36): smart integral caching, one variable?
 
         # safer version
-        if integral is not None:
-            integral = integral.get((limits, norm_range))
+        # if integral is not None:
+        #     integral = integral.get((limits, norm_range))
         # safer version end
 
         if integral is None:
-            integral = super()._single_hook_integrate(limits=limits, norm_range=norm_range, name=name)
+            integral_tensor = super()._single_hook_integrate(limits=limits, norm_range=norm_range, name=name)
             integral_holder = self._product_cache_integral_holder
-            assign_integral_op = integral_holder.assign(value=integral)
-            # self._cache['integral'] = integral_holder
+            assign_integral_op = integral_holder.assign(value=integral_tensor)
+            # TODO(Mayou36): fix integral cache, don't store holder, store value
+            self._cache['integral'] = integral_holder
             # safer version
-            self._cache['integral'][(limits, norm_range)] = integral_holder
+            # self._cache['integral'][(limits, norm_range)] = integral_holder
             # safer version end
+            # with tf.control_dependencies([integral_holder.initializer]):
             with tf.control_dependencies([assign_integral_op]):
-                integral = tf.identity(integral_holder)
+                integral = integral_holder.read_value()
 
         return integral
 
@@ -282,6 +289,7 @@ class SumAmplitudeSquaredPDF(zfit.pdf.BasePDF):
         to build the two-amplitude products. It defaults to :py:class:`AmplitudeProductCached`.
 
     """
+    _hack_use_default_sampling = False
 
     def __init__(self, obs, amp_list, coef_list, top_particle_mass,
                  name="SumAmplitudeSquaredPDF", external_integral=None,
@@ -299,8 +307,10 @@ class SumAmplitudeSquaredPDF(zfit.pdf.BasePDF):
         self._phsp = [amp.decay_phasespace() for amp in amp_list]
         self._top_at_rest = tf.stack((0.0, 0.0, 0.0, ztf.to_real(top_particle_mass)), axis=-1)
         super().__init__(obs=obs, name=name, params={coef.name: coef for coef in coef_list}, **kwargs)
-        self.update_integration_options(draws_per_dim=300000)
-        self._sample_and_weights = MethodType(generator_sample_and_weights_factory, self)
+        self.update_integration_options(draws_per_dim=30000)
+        # HACK: to do default accept reject with uniform sampling
+        if not self._hack_use_default_sampling:
+            self._sample_and_weights = MethodType(generator_sample_and_weights_factory, self)
 
     def _unnormalized_pdf(self, x):
         def unnormalized_pdf_func(x):
@@ -309,7 +319,8 @@ class SumAmplitudeSquaredPDF(zfit.pdf.BasePDF):
                                   axis=0)
             return ztf.to_real(value)
 
-        result = ztf.run_no_nan(unnormalized_pdf_func, x)
+        # result = ztf.run_no_nan(unnormalized_pdf_func, x)
+        result = unnormalized_pdf_func(x)
         return result
 
     def _do_sample(self, n_to_produce, limits):
@@ -353,9 +364,9 @@ class SumAmplitudeSquaredPDF(zfit.pdf.BasePDF):
         if external_integral is not None:
             integral = self._external_integral(limits=limits, norm_range=norm_range)
         else:
-            integral = tf.reduce_sum([2. * amp.integrate(limits=limits, norm_range=norm_range)
+            integral = tf.reduce_sum([2. * amp.integrate(limits=limits.get_subspace(amp.obs), norm_range=False)
                                       for amp in self._cross_terms] +
-                                     [amp.integrate(limits=limits, norm_range=norm_range)
+                                     [amp.integrate(limits=limits.get_subspace(amp.obs), norm_range=False)
                                       for amp in self._squared_terms],
                                      axis=0)
             integral = ztf.to_real(integral)
@@ -378,8 +389,9 @@ class Amplitude:
     """
 
     def __init__(self, decay_tree):  # noqa: W107
+        decay_tree = tuple(decay_tree)  # input validation, to make sure it's immutable
         if len(decay_tree) != 3:
-            raise KeyError("Badly specified decay tree")
+            raise KeyError("Badly specified decay tree, length has to be 3.")
         self._decay_tree = decay_tree
         self._top_mass = decay_tree[1]
 
@@ -413,7 +425,7 @@ class Amplitude:
         """Get a decay string.
 
         Arguments:
-            sanitize (bool, optional): Sanitize to conform to tensorflow variable names.
+            sanitize (bool, optional): Sanitize to conform to TensorFlow variable names.
 
         Return:
             str
@@ -494,9 +506,9 @@ class Resonance:
             :py:class:`~zfit.func.BaseFunc`
 
         """
-        args = self._model_config.copy()
-        args.update(extra_args)
-        return self._model(obs=obs, name=sanitize_string(name), **args)
+        kwargs = self._model_config.copy()
+        kwargs.update(extra_args)
+        return self._model(obs=obs, name=sanitize_string(name), **kwargs)
 
     def mass_sampler(self, name='', **extra_args):
         """Get mass sampler to use for phasespace generation.

zfit_amplitude/dynamics.py
@@ -21,7 +21,8 @@ def relativistic_breit_wigner(m2, mres, wres):
     Relativistic Breit-Wigner
     """
     # TODO: Check complex
-    below_div = (ztf.to_complex(mres ** 2 - m2) - tf.complex(ztf.constant(0.), mres) * ztf.to_complex(wres))
+    second_part = tf.complex(ztf.constant(0.), mres) * ztf.to_complex(wres)
+    below_div = ztf.to_complex(mres ** 2 - m2) - second_part
     # real_part = tf.real(below_div)
     # imag_part = tf.imag(below_div)
     # result = tf.complex(1. / real_part, 1. / imag_part)
@@ -56,7 +57,7 @@ class RelativisticBreitWigner(zfit.func.BaseFunc):
         zfit.run._enable_parameter_autoconversion = False
 
         super().__init__(obs=obs, name=name, dtype=dtype,
-                         params={'mres': mres, 'wres': mres})
+                         params={'mres': mres, 'wres': wres})
 
         zfit.run._enable_parameter_autoconversion = True
         # HACK end